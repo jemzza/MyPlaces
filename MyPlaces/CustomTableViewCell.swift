91b375a59df65e0764b1cd4b58f53bd49705d830
Alexander Litvinov <allidemocracy@gmail.com>
2020-02-15 16:28:14 +0300

    CustomTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Alexander on 15.02.2020.
+//  Copyright © 2020 Alexander Litvinov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    
+}
