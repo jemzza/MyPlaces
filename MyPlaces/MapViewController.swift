6f3e1ef399dbbe4ecea7aa42c4fe78829240b6a8
Alexander Litvinov <allidemocracy@gmail.com>
2020-02-19 21:10:38 +0300

    added Map View Controller setup placemarks

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Alexander on 19.02.2020.
+//  Copyright © 2020 Alexander Litvinov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    var place: Place!
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupPlacemark()
+    }
+    
+    
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func setupPlacemark() {
+        guard let location = place.location else { return }
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
+    }
+    
+}
